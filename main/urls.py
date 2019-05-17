d81d0d79031746a7c14c7f37e950bcaef192e38a
ayotycoon <38573905+ayotycoon@users.noreply.github.com>
2019-05-17 01:51:03 +0100

    make it better

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('user/<str:user>/', views.userDetails, name='userDetails'),
     path('add', views.add, name='add'),
+    (r'^static/(?P.*)$', 'django.views.static.serve',
+     {'document_root': settings.STATIC_ROOT}),
 ]
