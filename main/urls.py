f91b799f0f8a3bb7b2a48915587638db7e6c848d
ayotycoon <38573905+ayotycoon@users.noreply.github.com>
2019-05-17 01:56:47 +0100

    make it better

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
+from django.conf import settings
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('user/<str:user>/', views.userDetails, name='userDetails'),
     path('add', views.add, name='add'),
-    (r'^static/(?P.*)$', 'django.views.static.serve',
+    path(r'^static/(?P.*)$', 'django.views.static.serve',
      {'document_root': settings.STATIC_ROOT}),
 ]
