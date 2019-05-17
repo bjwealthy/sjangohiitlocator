38cd2fa56f772038fd330d44318051330f52b07c
ayotycoon <38573905+ayotycoon@users.noreply.github.com>
2019-05-17 01:37:36 +0100

    make it better

--- locator/settings.py
+++ locator/settings.py
@@ -27,13 +27,18 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['django-hiit.herokuapp.com']
 
+
+
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'staticfiles'),
-)
+
+print('-------------')
+print(PROJECT_ROOT)
+print('sssssssssssssssssss')
+print(BASE_DIR)
+
 # Application definition
 
 INSTALLED_APPS = [
